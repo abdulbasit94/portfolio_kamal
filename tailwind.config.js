e2335fdf5955852d6a2104b071fad59519cfb847
Kamaludin <kamaludinabdulbasit@gmail.com>
2023-09-09 16:14:12 +0700

    responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ export default {
     fontFamily:{
       'display' : ['DM SANS'],
       'body' : ['Helvetica']
-    }
+    },
+    screens: {
+      sm: "800px",
+      // => @media (min-width: 800px) { ... }
+
+      tablet: "801px",
+      // => @media (min-width: 801px) { ... }
+
+      desktop: "1200px",
+      // => @media (min-width: 1200px) { ... }
+    },
   },
   plugins: [],
 }
